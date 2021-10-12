d8c446438a5b879debd6f176ce04bf17c09f07f0
shihong <shihong@shihong.com>
2021-10-12 10:07:17 +0800

    debug

--- zx/collect.py
+++ zx/collect.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import sys,os
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from config import *
 from cardnews import CardNews
