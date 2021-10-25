132d3b32f7a07637078ef2f1626a649f07aa30d3
xiaowang <xw@xw.com>
2021-10-25 14:00:46 +0800

    history_pur

--- anastatistics/lianghao/history_purchase.py
+++ anastatistics/lianghao/history_purchase.py
@@ -8,6 +8,7 @@
 
 '''
 个人历史采购和采购数量按人统计按天分组
+每天凌晨统计数据 00:10:00开始跑
 '''
 
 import sys
@@ -17,17 +18,19 @@ sys.path.append("../../")
 from config import *
 from util.help_fun import *
 import json
+import datetime
+from datetime import timedelta,date
 
 
 conn_read = ssh_get_conn(lianghao_ssh_conf, lianghao_mysql_conf)
-sql = '''select phone,sum(total_price) total_money,sum(count) total_count from lh_order where del_flag = 0 and `status`=1 and phone !="" and phone is not null group by phone order by total_money desc limit 3'''
+sql = '''select phone,sum(total_price) total_money,sum(count) total_count,count(*) order_count from lh_order where del_flag = 0 and `status`=1 and phone !="" and phone is not null and DATE_FORMAT(pay_time,"%Y%m%d")  != CURRENT_DATE group by phone order by total_money desc'''
+
 datas = pd.read_sql(sql, conn_read)
 logger.info(datas)
 logger.info("-------")
 conn_read.close()
 
 #准备进入数据拼接获取用户信息 获取crm拼接 数据要一条一条查不然有出入 数据匹配不对
-
 crm_mysql_conf["db"] = "luke_sincerechat"
 conn_crm = direct_get_conn(crm_mysql_conf)
 logger.info(conn_crm)
@@ -48,23 +51,11 @@ with conn_crm.cursor() as cursor:
         else:
             pass
 
+datas["statistic_time"] = [date.today()+timedelta(days=-1)]*len(datas)
 conn_crm.close()
 
-
-#重新指定列
-# last_data = {}
-# last_data["unionid"] = datas["unionid"]
-# last_data["name"] = datas["name"]
-# last_data["nickname"] = datas["nickname"]
-# last_data["phone"] = datas["phone"]
-# last_data["total_money"] = datas["total_money"]
-# last_data["total_count"] = datas["total_count"]
-# last_data=pd.DataFrame(last_data)
-# logger.info(last_data)
-
+# 通过sqlclchemy创建的连接无需关闭
 logger.info("准备写入")
-# 通过sqlclchemy创建的连接无需
 conn_rw = ssh_get_sqlalchemy_conn(lianghao_ssh_conf,lianghao_rw_mysql_conf)
-datas.to_sql("lh_history_pur",con=conn_rw,if_exists="replace",index=False)
-# conn_rw.close()
+datas.to_sql("lh_history_pur",con=conn_rw,if_exists="append",index=False)
 logger.info("写入成功")
