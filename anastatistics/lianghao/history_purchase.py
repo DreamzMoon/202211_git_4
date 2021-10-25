682ae81c6186fb06eaff1cd8d6f85dce30d5c338
xiaowang <xw@xw.com>
2021-10-25 18:38:32 +0800

    debug

--- anastatistics/lianghao/history_purchase.py
+++ anastatistics/lianghao/history_purchase.py
@@ -15,6 +15,7 @@ import sys
 sys.path.append(".")
 sys.path.append("../")
 sys.path.append("../../")
+sys.path.append("../../../")
 from config import *
 from dataanalysis.config import *
 from util.help_fun import *
