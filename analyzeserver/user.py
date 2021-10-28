bd9fcecab1fc710ce715ffd566684eeade8c124d
shihong <shihong@shihong.com>
2021-10-28 12:50:31 +0800

    change sw_sql+script|config-message,add lukebus user operationcenter

--- analyzeserver/user.py
+++ analyzeserver/user.py
@@ -13,6 +13,7 @@ from flask import *
 from config import *
 import traceback
 from util.help_fun import *
+import time
 import datetime
 
 userbp = Blueprint('user', __name__, url_prefix='/user')
@@ -129,6 +130,125 @@ def crm_user_add():
         return {"code": "0000", "status": "success", "msg": u"crm数据同步成功"}
 
 
+    except:
+        logger.exception(traceback.format_exc())
+        return {"code":"10000","status":"failed","msg":message["10000"]}
+
+@userbp.route('/bus', methods=["POST"])
+def bus_user_add():
+    try:
+        # 接收参数
+        logger.info("新增用户")
+        logger.info(request.json)
+        phone = request.json.get('phone', "").strip()
+        # unionid = request.json.get('unionid', "").strip()
+        if not phone:
+            return {"code": "10001", "status": "failed", "msg": message["10001"]}
+
+        # 建立连接
+        crm_mysql_conf['db'] = 'luke_lukebus'
+        conn_bus = direct_get_conn(crm_mysql_conf)
+        if not conn_bus:
+            return {"code": "10002", "status": "failed", "msg": message["10002"]}
+        # 获取禄可商务用户信息sql
+        bus_user_find_sql = '''
+            select
+                t1.unionid, t1.punionid parentid, t1.phone, t1.u_nickname nickname, t1.capacity, t1.`exclusive`, t1.`status` bus_status,
+                FROM_UNIXTIME(t1.addtime,'%%Y-%%m-%%d %%H:%%i:%%s') addtime, FROM_UNIXTIME(t1.udptime,'%%Y-%%m-%%d %%H:%%i:%%s') udptime,
+                FROM_UNIXTIME(t1.invtime,'%%Y-%%m-%%d %%H:%%i:%%s') invtime, t2.identity, t2.types auto_type, t2.`status` vertify_status,
+                t2.sex, t2.`name`, t2.address, t2.birth, t2.nationality, FROM_UNIXTIME(t2.extime,'%%Y-%%m-%%d %%H:%%i:%%s') extime,
+                t3.grade provider_garde, t3.`status` provider_status, FROM_UNIXTIME(t3.starttime,'%%Y-%%m-%%d %%H:%%i:%%s') provider_starttime,
+                t4.grade vip_grade, FROM_UNIXTIME(t4.starttime,'%%Y-%%m-%%d %%H:%%i:%%s')  vip_starttime, FROM_UNIXTIME(t4.endtime,'%%Y-%%m-%%d %%H:%%i:%%s')  vip_endtime
+            from
+                    luke_lukebus.`user` t1
+            left join
+                luke_crm.authentication t2
+            on
+                t1.unionid = t2.unionid
+            left join
+                luke_crm.user_serpro t3
+            on
+                t2.unionid = t3.unionid
+            left join
+                luke_crm.user_vip t4
+            on
+                t3.unionid = t4.unionid
+            where
+                t1.phone = %s
+        '''
+        with conn_bus.cursor() as cursor:
+            cursor.execute(bus_user_find_sql, (phone,))
+            bus_user_data = cursor.fetchone()
+        conn_bus.close()
+        if not bus_user_data:
+            return {"code": "10003", "status": "failed", "msg": message["10003"]}
+        logger.info(bus_user_data)
+
+        # 禄可商户用户插入列
+        bus_user_param = [bus_user_data['unionid'], bus_user_data['parentid'], bus_user_data['name'], bus_user_data['nickname'], bus_user_data['sex'], bus_user_data['phone'],
+                     bus_user_data['identity'], bus_user_data['auto_type'], bus_user_data['vertify_status'], bus_user_data['address'], bus_user_data['birth'], bus_user_data['nationality'], bus_user_data['capacity'],
+                     bus_user_data['exclusive'], bus_user_data['bus_status'], bus_user_data['provider_garde'], bus_user_data['provider_status'], bus_user_data['provider_starttime'], bus_user_data['vip_grade'], bus_user_data['vip_starttime'],
+                     bus_user_data['vip_endtime'], bus_user_data['addtime'], bus_user_data['udptime'], bus_user_data['invtime'], bus_user_data['extime'], 0, time.strftime("%Y-%m-%d %H:%M:%S")]
+        logger.info(bus_user_param)
+
+        # 数据同步
+        conn_bus_rw = ssh_get_conn(lianghao_ssh_conf, lianghao_rw_mysql_conf)
+        with conn_bus_rw.cursor() as cursor:
+            bus_user_insert_sql = '''insert into bus_user values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+            cursor.execute(bus_user_insert_sql, bus_user_param)
+        conn_bus_rw.commit()
+        conn_bus_rw.close()
+        return {"code": "0", "status": "success", "msg": message["0"]}
+    except:
+        logger.exception(traceback.format_exc())
+        return {"code": "10000", "status": "failed", "msg": message["10000"]}
+
+
+@userbp.route('/operationcenter', methods=["POST"])
+def bus_operationcenter_add():
+    try:
+        logger.info('新增运营中心')
+        logger.info(request.json)
+        #　接收参数
+        unionid = request.json.get('unionid', "").strip()
+        if not unionid:
+            return {"code": "10001", "status": "failed", "msg": message["10001"]}
+
+        # 建立连接
+        crm_mysql_conf['db'] = 'luke_lukebus'
+        conn_bus_center = direct_get_conn(crm_mysql_conf)
+        if not conn_bus_center:
+            return {"code": "10002", "status": "failed", "msg": message["10002"]}
+
+        # 获取禄可商务运营中心
+        center_find_sql = '''
+            select
+                unionid, punionid parentid, capacity, `name`, telephone phone, operatename, is_factory, status, crm, from_unixtime(addtime, '%%Y-%%m-%%d %%H:%%i:%%s') addtime
+            from
+                luke_lukebus.operationcenter
+            where
+                unionid = %s
+            '''
+        with conn_bus_center.cursor() as cursor:
+            cursor.execute(center_find_sql, (unionid,))
+            bus_center_data = cursor.fetchone()
+        conn_bus_center.close()
+        if not bus_center_data:
+            return {"code": "10003", "status": "failed", "msg": message["10003"]}
+
+        # 禄可商务运营中心插入列
+        bus_center_param = [bus_center_data['unionid'], bus_center_data['parentid'], bus_center_data['capacity'], bus_center_data['name'],
+                            bus_center_data['phone'], bus_center_data['operatename'], bus_center_data['is_factory'], bus_center_data['status'],
+                            bus_center_data['crm'], bus_center_data['addtime'], time.strftime("%Y-%m-%d %H:%M:%S")]
+
+        # 数据同步
+        conn_bus_rw = ssh_get_conn(lianghao_ssh_conf, lianghao_rw_mysql_conf)
+        with conn_bus_rw.cursor() as cursor:
+            bus_center_insert_sql = '''insert into bus_operationcenter values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+            cursor.execute(bus_center_insert_sql, bus_center_param)
+        conn_bus_rw.commit()
+        conn_bus_rw.close()
+        return {"code": "0", "status": "success", "msg": message["0"]}
     except:
         logger.exception(traceback.format_exc())
         return {"code": "10000", "status": "failed", "msg": message["10000"]}
