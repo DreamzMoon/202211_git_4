1c580c409c3f623ed3c38964a010b9e7db341487
wangyuting <wyt123456>
2021-11-24 15:15:25 +0800

    debug

--- analyzeserver/__init__.py
+++ analyzeserver/__init__.py
@@ -21,6 +21,9 @@ CORS(app, supports_credentials=True)
 def hello():
     return 'Hello, World!'
 
+@app.route("/hello")
+def hellohello():
+    return "hello xiaowang"
 
 from analyzeserver.other.user import userbp
 app.register_blueprint(userbp)
