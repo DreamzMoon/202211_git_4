9e0ee3c69bb51a925c120ab157f0758c5a0e4e99
wangyuting <wyt123456>
2021-11-25 11:57:36 +0800

    home

--- analyzeserver/home/home_page.py
+++ analyzeserver/home/home_page.py
@@ -258,7 +258,7 @@ def deal_top():
         left join lh_sell s on o.sell_id = s.id
         where DATE_FORMAT(o.create_time,"%Y%m%d") = CURRENT_DATE 
         and o.del_flag = 0 and o.type in (1,4) and o.`status` = 1 
-        order by o.create_time desc) t group by pretty_type_name'''
+        order by o.create_time desc) t group by pretty_type_name order by total_price desc'''
         data = (pd.read_sql(sql, conn_lh)).to_dict("records")
         return {"code": "0000", "status": "success", "msg": data}
 
