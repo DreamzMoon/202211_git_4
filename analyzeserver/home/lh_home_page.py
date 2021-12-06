8ca61dae483601b5abca3bffc75cebcedb163ea2
wangyuting <wyt123456>
2021-12-06 14:29:35 +0800

    首页7-8位总销售

--- analyzeserver/home/lh_home_page.py
+++ analyzeserver/home/lh_home_page.py
@@ -197,12 +197,23 @@ def deal_top():
         conn_lh = direct_get_conn(lianghao_mysql_conf)
 
 
-        sql = '''select pretty_type_name,unit_price,sum(count) total_count,sum(total_price) total_price from (
+        sql = '''(select pretty_type_name,unit_price,sum(count) total_count,sum(total_price) total_price from (
+        select s.pretty_type_name,o.unit_price,o.count,o.total_price from le_order o
+        left join (select id,pretty_type_name from le_sell 
+        union all
+        select id,pretty_type_name from le_second_hand_sell) s on o.sell_id = s.id
+        where DATE_FORMAT(o.create_time,"%Y%m%d") = CURRENT_DATE()
+        and o.del_flag = 0 and o.type in (1,4) and o.`status` = 1
+        order by o.create_time desc) t group by pretty_type_name  order by total_count desc)
+        union 
+        (select pretty_type_name,unit_price,sum(count) total_count,sum(total_price) total_price from (
         select s.pretty_type_name,o.unit_price,o.count,o.total_price from lh_order o
         left join lh_sell s on o.sell_id = s.id
-        where DATE_FORMAT(o.create_time,"%Y%m%d") = CURRENT_DATE
+        where DATE_FORMAT(o.create_time,"%Y%m%d") = CURRENT_DATE()
         and o.del_flag = 0 and o.type in (1,4) and o.`status` = 1
-        order by o.create_time desc) t group by pretty_type_name order by total_count desc'''
+        order by o.create_time desc) t group by pretty_type_name order by total_count desc)
+        order by total_count desc
+        '''
 
 
         data = (pd.read_sql(sql, conn_lh)).to_dict("records")
