63c06aed5811107960291215cda241e6ae4d5fdf
wangyuting <wyt123456>
2021-12-30 14:39:40 +0800

    shouye

--- analyzeserver/home/lh7_home_page.py
+++ analyzeserver/home/lh7_home_page.py
@@ -60,7 +60,7 @@ def deal_person():
         logger.info(phone_lists)
         if not phone_lists:
             return {"code":"0000","status":"success","msg":[]}
-        sql = '''select phone,if(`name` is not null,`name`,if(nickname is not null,nickname,"")) username from crm_user_{} where phone in ({})'''.format(current_time,",".join(phone_lists))
+        sql = '''select phone,if(`name` is not null,`name`,if(nickname is not null,nickname,"")) username from crm_user where phone in ({})'''.format(",".join(phone_lists))
         logger.info(sql)
         user_data = pd.read_sql(sql,conn_analyze)
         datas = datas.merge(user_data,on="phone",how="left")
@@ -372,8 +372,8 @@ def today_dynamic_transaction():
         #     '''
 
         search_name_sql = '''
-                        select phone, if(`name` is not null,`name`,if(nickname is not null,nickname,"")) username from crm_user_{} where phone = "%s"
-                    '''.format(current_time)
+                        select phone, if(`name` is not null,`name`,if(nickname is not null,nickname,"")) username from crm_user where phone = "%s"
+                    '''
 
         order_df = pd.read_sql(sell_order_sql, conn_lh)
         if order_df.shape[0] > 0:
@@ -441,8 +441,8 @@ def today_dynamic_publish():
         #         select phone, if(`name` is not null,`name`,if(nickname is not null,nickname,"")) username from luke_sincerechat.user where phone = "%s"
         #     '''
         search_name_sql = '''
-                        select phone, if(`name` is not null,`name`,if(nickname is not null,nickname,"")) username from crm_user_{} where phone = "%s"
-                    '''.format(current_time)
+                        select phone, if(`name` is not null,`name`,if(nickname is not null,nickname,"")) username from crm_user where phone = "%s"
+                    '''
 
         # 发布时时动态
         publish_order_sql = '''
@@ -527,8 +527,8 @@ def today_dynamic_newuser():
         #     '''
 
         search_name_sql = '''
-                        select phone, if(`name` is not null,`name`,if(nickname is not null,nickname,"")) username from crm_user_{} where phone = "%s"
-                    '''.format(current_time)
+                        select phone, if(`name` is not null,`name`,if(nickname is not null,nickname,"")) username from crm_user where phone = "%s"
+                    '''
 
         # 新注册用户
         new_user_sql = '''
