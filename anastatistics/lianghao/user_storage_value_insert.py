037ec28dbbf365fc9d9c9f82f9d938ec181cd624
wangyuting <wyt123456>
2022-01-20 15:53:20 +0800

    fixbug

--- anastatistics/lianghao/user_storage_value_insert.py
+++ anastatistics/lianghao/user_storage_value_insert.py
@@ -146,7 +146,7 @@ def tran_hold():
     try:
         conn_lh = direct_get_conn(lianghao_mysql_conf)
         current_time = datetime.datetime.now()
-        ergodic_time = current_time.strftime("%Y-%m-%d %H:%M:S")
+        ergodic_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
         # ergodic_time = datetime.datetime.now().strftime("%Y-%m-%d")
 
 
