f0b8bf809cf6b78eb478666d520aad2c8ed7064a
wangyuting <wyt123456>
2022-01-20 11:22:19 +0800

    plat

--- anastatistics/lianghao/plat_lh_count_78.py
+++ anastatistics/lianghao/plat_lh_count_78.py
@@ -45,6 +45,30 @@ select count(*) count from lh_free_pretty_9
 ) t
 '''
 
+eight_sql = '''select sum(count) eight_count from(
+select count(*) count from le_pretty_0 union all
+select count(*) count from le_pretty_1 union all
+select count(*) count from le_pretty_2 union all
+select count(*) count from le_pretty_3 union all
+select count(*) count from le_pretty_4 union all
+select count(*) count from le_pretty_5 union all
+select count(*) count from le_pretty_6 union all
+select count(*) count from le_pretty_7 union all
+select count(*) count from le_pretty_8 union all
+select count(*) count from le_pretty_9 union all 
+select count(*) count from le_free_pretty_0 union all
+select count(*) count from le_free_pretty_1 union all
+select count(*) count from le_free_pretty_2 union all
+select count(*) count from le_free_pretty_3 union all
+select count(*) count from le_free_pretty_4 union all
+select count(*) count from le_free_pretty_5 union all
+select count(*) count from le_free_pretty_6 union all
+select count(*) count from le_free_pretty_7 union all
+select count(*) count from le_free_pretty_8 union all
+select count(*) count from le_free_pretty_9
+) t
+'''
+
 
 try:
     conn_lh = direct_get_conn(lianghao_mysql_conf)
@@ -53,6 +77,13 @@ try:
     plat_total_count_7 = int(plat_total_count_seven_data["seven_count"][0])
     r = get_redis()
     r.set("plat_lh_total_count_seven",plat_total_count_7)
+
+    plat_total_count_eight_data = pd.read_sql(eight_sql, conn_lh)
+    logger.info(plat_total_count_eight_data)
+    plat_total_count_8 = int(plat_total_count_eight_data["eight_count"][0])
+    r = get_redis()
+    r.set("plat_lh_total_count_eight", plat_total_count_8)
+
 except:
     logger.info("异常")
 finally:
