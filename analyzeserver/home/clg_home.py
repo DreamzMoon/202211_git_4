cfed03538227d06379903fa2e6f215716f56949c
wangyuting <wyt123456>
2022-01-25 15:57:18 +0800

    clghome

--- analyzeserver/home/clg_home.py
+++ analyzeserver/home/clg_home.py
@@ -49,8 +49,9 @@ def person_top():
 
 
 
-        sql = '''select phone,sum(pay_money) total_money from trade_order_info where DATE_FORMAT(create_time,"%Y-%m_%d") = CURRENT_DATE and order_status in (4,5,6)
-        group by phone order by total_money desc limit 3'''
+        sql = '''select phone,sum(pay_money) pay_total_money from trade_order_info where DATE_FORMAT(create_time,"%Y-%m_%d") = CURRENT_DATE and order_status in (4,5,6,10,15)
+        and del_flag = 0
+        group by phone order by pay_total_money desc limit 3'''
         logger.info(sql)
         datas = pd.read_sql(sql, conn_clg)
         # datas = datas.to_dict("records")
@@ -104,11 +105,11 @@ def product_top():
 
 
 
-        sql = '''select od.goods_name,sum(o.total_money) total_money from trade_order_info o
+        sql = '''select od.goods_name,sum(o.pay_money) pay_total_money from trade_order_info o
         left join trade_order_item od on o.order_sn = od.order_sn
-        where DATE_FORMAT(o.create_time,"%Y-%m_%d") = CURRENT_DATE and o.order_status in (4,5,6)
+        where DATE_FORMAT(o.create_time,"%Y-%m_%d") = CURRENT_DATE and o.order_status in (4,5,6,10,15) and o.del_flag = 0 
         group by od.goods_id
-        order by o.create_time desc limit 3'''
+        order by pay_total_money desc limit 3'''
         logger.info(sql)
         datas = pd.read_sql(sql, conn_clg)
         datas = datas.to_dict("records")
@@ -148,14 +149,69 @@ def shop_top():
 
 
 
-        sql = '''select o.shop_name,sum(o.total_money) total_money from trade_order_info o
-        where DATE_FORMAT(o.create_time,"%Y-%m_%d") = CURRENT_DATE and o.order_status in (4,5,6)
+        sql = '''select o.shop_name,sum(o.pay_money) pay_total_money from trade_order_info o
+        where DATE_FORMAT(o.create_time,"%Y-%m_%d") = CURRENT_DATE and o.order_status in (4,5,6,10,15) and o.del_flag = 0 
         group by o.shop_id
-        order by o.create_time desc limit 7'''
+        order by pay_total_money desc limit 7'''
         logger.info(sql)
         datas = pd.read_sql(sql, conn_clg)
         datas = datas.to_dict("records")
 
+
+        return {"code": "0000", "status": "success", "msg": datas}
+
+    except Exception as e:
+        logger.exception(traceback.format_exc())
+        return {"code": "10000", "status": "failed", "msg": message["10000"]}
+    finally:
+        conn_clg.close()
+        conn_analyze.close()
+
+
+@clghomebp.route("order/status",methods=["GET"])
+def order_status():
+    try:
+        conn_clg = direct_get_conn(clg_mysql_conf)
+        logger.info(conn_clg)
+        conn_analyze = direct_get_conn(analyze_mysql_conf)
+        cursor = conn_analyze.cursor()
+        try:
+            logger.info("env:%s" % ENV)
+            token = request.headers["Token"]
+            user_id = request.args.get("user_id")
+
+            if not user_id and not token:
+                return {"code": "10001", "status": "failed", "msg": message["10001"]}
+
+            check_token_result = check_token(token, user_id)
+            if check_token_result["code"] != "0000":
+                return check_token_result
+        except:
+            return {"code": "10004", "status": "failed", "msg": message["10004"]}
+
+
+
+        sql = '''select o.phone,o.pay_money,od.goods_name from trade_order_info o
+        left join trade_order_item od on o.order_sn = od.order_sn
+        where DATE_FORMAT(o.create_time,"%Y-%m_%d") = CURRENT_DATE and o.order_status in (4,5,6,10,15) and o.del_flag = 0 
+        order by o.create_time desc limit 3'''
+        logger.info(sql)
+        datas = pd.read_sql(sql, conn_clg)
+        # datas = datas.to_dict("records")
+        # logger.info(len(datas))
+        phone_lists = datas["phone"].tolist()
+
+        logger.info(phone_lists)
+        if not phone_lists:
+            return {"code": "0000", "status": "success", "msg": []}
+        sql = '''select phone,if(`name` is not null,`name`,if(nickname is not null,nickname,"")) username from crm_user where phone in ({})'''.format(
+            ",".join(phone_lists))
+        logger.info(sql)
+        user_data = pd.read_sql(sql, conn_analyze)
+        datas = datas.merge(user_data, on="phone", how="left")
+        logger.info(datas)
+        datas["username"].fillna("", inplace=True)
+        datas = datas.to_dict("records")
 
         return {"code": "0000", "status": "success", "msg": datas}
 
