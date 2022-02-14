fdd7d6411c49fd2d9681e82d814de879fbc49fd7
wangyuting <wyt123456>
2022-02-14 17:42:17 +0800

    debug

--- analyzeserver/clg/good_tran.py
+++ analyzeserver/clg/good_tran.py
@@ -76,8 +76,7 @@ def clg_tran_good_all():
         serach_phone = list(set(shop_data["phone"].to_list()))
         serach_phone.remove("___________")
         # crm_sql = '''select unionid,if(`name` is not null and `name`!='',`name`,if(nickname is not null,nickname,"")) nickname,phone from crm_user where del_flag = 0 and phone is not null and phone != ""'''
-        crm_sql = '''select unionid,if(`name` is not null and `name`!='',`name`,if(nickname is not null,nickname,"")) nickname,phone from crm_user where del_flag = 0 and phone is not null and phone != "" and phone in (%s)''' % (
-            ",".join(serach_phone))
+        crm_sql = '''select unionid,if(`name` is not null and `name`!='',`name`,if(nickname is not null,nickname,"")) nickname,phone from crm_user where del_flag = 0 and phone is not null and phone != "" and phone in (%s)''' % (",".join(serach_phone))
 
         # crm_sql = '''select unionid,if(`name` is not null and `name`!='',`name`,if(nickname is not null,nickname,"")) nickname,phone from crm_user where del_flag = 0 and phone is not null and phone != ""'''
         crm_data = pd.read_sql(crm_sql,conn_analyze)
