ccc60e1dfcd73376268038e3578577321b394454
wangyuting <wyt123456>
2022-02-15 09:45:20 +0800

    debug

--- analyzeserver/clg/good_tran.py
+++ analyzeserver/clg/good_tran.py
@@ -74,11 +74,10 @@ def clg_tran_good_all():
         logger.info("店铺数据读取完成")
 
         serach_phone = list(set(shop_data["phone"].to_list()))
-        serach_phone.remove("___________")
-        # crm_sql = '''select unionid,if(`name` is not null and `name`!='',`name`,if(nickname is not null,nickname,"")) nickname,phone from crm_user where del_flag = 0 and phone is not null and phone != ""'''
+        if "___________" in serach_phone:
+            serach_phone.remove("___________")
         crm_sql = '''select unionid,if(`name` is not null and `name`!='',`name`,if(nickname is not null,nickname,"")) nickname,phone from crm_user where del_flag = 0 and phone is not null and phone != "" and phone in (%s)''' % (",".join(serach_phone))
 
-        # crm_sql = '''select unionid,if(`name` is not null and `name`!='',`name`,if(nickname is not null,nickname,"")) nickname,phone from crm_user where del_flag = 0 and phone is not null and phone != ""'''
         crm_data = pd.read_sql(crm_sql,conn_analyze)
         logger.info("用户数据完成")
 
