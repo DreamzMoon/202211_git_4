8932f3631a9269a46befc7bebede6483b439603c
wangyuting <wyt123456>
2022-02-28 15:46:14 +0800

    debug

--- analyzeserver/clm/transfer.py
+++ analyzeserver/clm/transfer.py
@@ -450,41 +450,47 @@ def clm_orderflow_all():
         if order_status != '':
             base_order_sql += ''' and order_status=%s''' % order_status
         # 购买人
-        # if buyer_info != '':
-        #     buyer_resutl = get_phone_by_keyword(buyer_info)
-        #     if not buyer_resutl[0]:
-        #         return {"code": "0000", "status": "success", "msg": [], "count": 0}
-        #     buyer_phone_list = buyer_resutl[1]
-        #     user_info_sql += ''' and phone in (%s)''' % ','.join(buyer_phone_list)
-        #
-        #     user_info_df = pd.read_sql(user_info_sql, conn_analyze)
-        #     unionid_list = [str(unionid) for unionid in user_info_df['unionid'].tolist()]
-        #     base_order_sql += ''' and unionid in (%s)''' % ','.join(unionid_list)
-        #     user_order_df = pd.read_sql(base_order_sql,conn_crm)
-        #
-        # else:
-        #     user_order_df = pd.read_sql(base_order_sql, conn_crm)
-        #     unionid_list = [str(unionid) for unionid in set(user_order_df['unionid'].tolist())]
-        #     if len(unionid_list) == 0:
-        #         return {"code": "0000", "status": "success", "msg": [], "count": 0}
-        #     user_info_sql += ''' and unionid in (%s)''' % ','.join(unionid_list)
-        #     user_info_df = pd.read_sql(user_info_sql, conn_analyze)
+        if buyer_info != '':
+            # buyer_resutl = get_phone_by_keyword(buyer_info)
+            # if not buyer_resutl[0]:
+            #     return {"code": "0000", "status": "success", "msg": [], "count": 0}
+            # buyer_phone_list = buyer_resutl[1]
+            # user_info_sql += ''' and phone in (%s)''' % ','.join(buyer_phone_list)
+            user_info_sql = '''
+                        select * from (
+                        select unionid,phone,if(`name` is not null and `name`!='',`name`,if(nickname is not null,nickname,"")) nickname
+                         from crm_user where phone like "%%%s%%" or unionid like "%%%s%%" or `name` like "%%%s%%" or nickname like "%%%s%%") t 
+                        where t.phone is not null and (t.nickname like "%%%s%%" or phone like "%%%s%%" or unionid like "%%%s%%")
+                        ''' % (buyer_info, buyer_info, buyer_info, buyer_info, buyer_info, buyer_info, buyer_info)
+
+            user_info_df = pd.read_sql(user_info_sql, conn_analyze)
+            unionid_list = [str(unionid) for unionid in user_info_df['unionid'].tolist()]
+            base_order_sql += ''' and unionid in (%s)''' % ','.join(unionid_list)
+            user_order_df = pd.read_sql(base_order_sql,conn_crm)
+
+        else:
+            user_order_df = pd.read_sql(base_order_sql, conn_crm)
+            unionid_list = [str(unionid) for unionid in set(user_order_df['unionid'].tolist())]
+            if len(unionid_list) == 0:
+                return {"code": "0000", "status": "success", "msg": [], "count": 0}
+            user_info_sql += ''' and unionid in (%s)''' % ','.join(unionid_list)
+            user_info_df = pd.read_sql(user_info_sql, conn_analyze)
 
         #  购买人
-        if buyer_info:
-            user_info_sql = '''
-            select * from (
-            select unionid,phone,if(`name` is not null and `name`!='',`name`,if(nickname is not null,nickname,"")) nickname
-             from crm_user where phone like "%%%s%%" or unionid like "%%%s%%" or `name` like "%%%s%%" or nickname like "%%%s%%") t 
-            where t.phone is not null and (t.nickname like "%%%s%%" or phone like "%%%s%%" or unionid like "%%%s%%")
-            ''' %(buyer_info,buyer_info,buyer_info,buyer_info,buyer_info,buyer_info,buyer_info)
-            logger.info(user_info_sql)
-        user_order_df = pd.read_sql(base_order_sql, conn_crm)
-        unionid_list = [str(unionid) for unionid in set(user_order_df['unionid'].tolist())]
-        if len(unionid_list) == 0:
-            return {"code": "0000", "status": "success", "msg": [], "count": 0}
-        user_info_sql += ''' and unionid in (%s)''' % ','.join(unionid_list)
-        user_info_df = pd.read_sql(user_info_sql, conn_analyze)
+        # if buyer_info:
+        #     user_info_sql = '''
+        #     select * from (
+        #     select unionid,phone,if(`name` is not null and `name`!='',`name`,if(nickname is not null,nickname,"")) nickname
+        #      from crm_user where phone like "%%%s%%" or unionid like "%%%s%%" or `name` like "%%%s%%" or nickname like "%%%s%%") t
+        #     where t.phone is not null and (t.nickname like "%%%s%%" or phone like "%%%s%%" or unionid like "%%%s%%")
+        #     ''' %(buyer_info,buyer_info,buyer_info,buyer_info,buyer_info,buyer_info,buyer_info)
+        #     logger.info(user_info_sql)
+        # user_order_df = pd.read_sql(base_order_sql, conn_crm)
+        # unionid_list = [str(unionid) for unionid in set(user_order_df['unionid'].tolist())]
+        # if len(unionid_list) == 0:
+        #     return {"code": "0000", "status": "success", "msg": [], "count": 0}
+        # user_info_sql += ''' and unionid in (%s)''' % ','.join(unionid_list)
+        # user_info_df = pd.read_sql(user_info_sql, conn_analyze)
 
 
         # 合并用户信息与订单信息
