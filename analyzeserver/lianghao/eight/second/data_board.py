2e224ead0ca8e3d2fead1fb2f04baf5ff6593120
wangyuting <wyt123456>
2022-03-08 11:32:53 +0800

    debug

--- analyzeserver/lianghao/eight/second/data_board.py
+++ analyzeserver/lianghao/eight/second/data_board.py
@@ -68,15 +68,15 @@ def le_secboard_sell():
             return {"code": "11036", "status": "failed", "msg": message["11036"]}
 
         #上架总数
-        sell_sum_count_sql = '''select sum(count) sell_count from le_second_hand_sell where `status` != 1 and del_flag = 0'''
+        sell_sum_count_sql = '''select sum(count) sell_count from le_second_hand_sell where `status` = 0 and del_flag = 0'''
 
         #上架价值
-        sell_sum_price_sql = '''select sum(total_price) sell_total_price from le_second_hand_sell where `status` != 1 and del_flag = 0'''
+        sell_sum_price_sql = '''select sum(total_price) sell_total_price from le_second_hand_sell where `status` = 0 and del_flag = 0'''
 
         #内部渠道上架总数
-        inside_sell_count_sql = '''select sum(count) inside_sell_count from le_second_hand_sell where `status` != 1 and del_flag = 0 and sell_phone in (%s)''' %(kanban_data[0]["inside_publish_phone"][1:-1])
+        inside_sell_count_sql = '''select sum(count) inside_sell_count from le_second_hand_sell where `status` = 0 and del_flag = 0 and sell_phone in (%s)''' %(kanban_data[0]["inside_publish_phone"][1:-1])
         # 内部渠道商家价值
-        inside_sell_price_sql = '''select sum(total_price) inside_sell_total_price from le_second_hand_sell where `status` != 1 and del_flag = 0 and sell_phone in (%s)''' % (kanban_data[0]["inside_publish_phone"][1:-1])
+        inside_sell_price_sql = '''select sum(total_price) inside_sell_total_price from le_second_hand_sell where `status` = 0 and del_flag = 0 and sell_phone in (%s)''' % (kanban_data[0]["inside_publish_phone"][1:-1])
 
         #判断是否有官方号
         if kanban_data[0]["inside_publish_phone"][1:-1]:
